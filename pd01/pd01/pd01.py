a01ef150d557b60d8ac02f7eb6b8b56db364417b
NaeunJung <coco941@naver.com>
2015-09-02 13:48:52 +0900

    0902lab
    형변환,  숫자형, 문자열 etc

---
+++ pd01/pd01/pd01.py
@@ -0,0 +1,37 @@
+﻿#print('I have %.2f cats' % 6)
+#print('I have %03d cats' % 6)
+#print('I have %d cats and %d dogs'%(5,6))          ctrl+kc:여러줄 주석처리 ctrl+ku 주석해제
+
+
+#program example 고급설정옵션->utf8으로(한글가능)
+#print('한글')
+#salary = input("Please enter your salary:")
+#bonus = input("Please enter your bonus:")
+#payCheck = salary+bonus
+#payCheck = float(salary) + float(bonus)
+#print(payCheck)
+#print(type(payCheck))  //타입확인 
+
+#name="greenjoa"
+#print(name[7])
+
+#info="201311246NaeunJung"
+#sid=info[:9]
+#sname=info[9:]
+#print(sid+" "+sname)
+#print('학번: '+sid)
+#print('이름: '+sname)
+
+#a="I eat %s apples." % "five"
+#print(a)
+#a="I eat %10s apples." % "five" //five후에 한칸 간격
+#print(a)
+#a="I eat %-10s apples." % "five" //five전에 한칸 간격
+#print(a)
+#a ="Error is %d%%." %98  // 뒤에 %를 붙히기 위해 %2개
+#print(a)
+
+
+res = input("end")
+print(res.upper())
+
