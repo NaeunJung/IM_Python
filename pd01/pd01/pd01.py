66d5645f176a33334a4bc1d5da9f88456a2cac5a
NaeunJung <coco941@naver.com>
2015-09-07 13:52:40 +0900

    0907_두번째 실습자료
    리스트 실습

--- pd01/pd01/pd01.py
+++ pd01/pd01/pd01.py
@@ -35,3 +35,4 @@
 res = input("end")
 print(res.upper())
 
+
---
+++ pd02/pd02/pd02.py
@@ -0,0 +1,60 @@
+﻿#e=[1,2,['Life','is']]
+#print(e[0])
+#print(e[-1])
+
+#b=[1,2,3]
+#c=['Life','is','too','short']
+#f=b+c 
+#g=b*3    
+#print(f)
+#print(g)
+
+scores=[90,55,78,85,100]
+#print(score[-4])
+
+#for steps in range(3):
+#  print(scores.sort())
+
+#num=scores.pop()
+#num2=scores.pop(2)
+#print(num2)
+#print(scores)
+
+#num=scores.count(85)
+#print(num)
+
+guests=['a','b','c','d']
+#guests[0]='greenjoa'
+#guests[1]=['greenjoa1','greenjoa2']
+#guests[1:2]=['greenjoa1','greenjoa2']
+#guests.insert(2,'e')
+#print(guests)
+
+#for steps in guests:
+#    if isinstance(steps.list) :
+#        for step in steps :
+#           print(step)
+#    else :
+#        print(steps)  
+    #//range값에 있는 index수 만큼만 for문으로 출력된다.
+
+#for steps in guests:
+#    print(steps)    //guests안에 있는 값들 소진될때까지 for문 돌림.
+        
+
+#id=['mickey','mini','donald']
+#id.insert(1,'1234')
+#id.insert(4,'5678')
+#id.insert(6,'3489')
+#id.insert(2,'[010-1234-3452]')
+#id.insert(5,'[010-2398-4582]')
+#id.insert(7,'[010-2310-1234]')
+#print(id)
+
+#투플
+t1=()
+t3=(1,2,3)
+t4=1,2,3
+t5=('a','b',('ab','cd'))
+print(t4)
+print(t5)
